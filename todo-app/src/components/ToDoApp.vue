7fcd8d41eb5d754d00304d72ac1f2b65aae5d2c6
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-25 20:36:55 +0200

    template done, task functions fixed

--- todo-app/src/components/ToDoApp.vue
+++ todo-app/src/components/ToDoApp.vue
@@ -1,39 +1,38 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
 
-const newTask = ref('')
-const tasks = ref([])
+const newTask = ref("");
+const tasks = ref([]);
 
 const addTask = () => {
-    if(newTask.value.trim() !== ''){
-        tasks.value.push(newTask.value)
-        newTask.value =''
-    }
-}
+  if (newTask.value.trim() !== "") {
+    tasks.value.push(newTask.value);
+    newTask.value = "";
+  }
+};
 
 const removeTask = (index) => {
-    tasks.value.splice(index, 1)
-}
-
-
-
+  tasks.value.splice(index, 1);
+};
 </script>
 
 <template>
-<div class="todo-app">
+  <div class="todo-app">
     <div class="task-input">
-        <input v-model="newTask" @keyup.enter="addTask" placeholder="add your task here"/>
-        <button @click="addTask">Add task</button>
+      <input
+        v-model="newTask"
+        @keyup.enter="addTask"
+        placeholder="add your task here"
+      />
+      <button @click="addTask">Add task</button>
     </div>
     <ul>
-        <li v-for="(task, index) in tasks" key="index">
-            {{ task }}
-        </li>
-        <button @click="removeTask">Remove</button>
+      <li v-for="(task, index) in tasks" key="index" class="task-item">
+        {{ task }}
+        <button @click="removeTask" class="remove-button">Remove</button>
+      </li>
     </ul>
-</div>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
