fa0030894b7e70b7ea47be784208bf5b7dea3670
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-25 23:34:31 +0200

    appendtodisplay function

---
+++ amazing-calculator/src/components/AmazingCalculator.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {ref, computed} from 'vue'
+
+const display = ref('0')
+
+const appendToDisplay = (value) => {
+    if(display.value === '0' && value !== '.'){
+        display.value = value
+    } else {
+        display.value += value;
+    }
+}
+
+</script>
