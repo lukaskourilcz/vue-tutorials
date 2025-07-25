4304c3423903250a291b8b85d6c8b9092b912b6c
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-25 23:44:13 +0200

    calculate + displayclass function created

--- amazing-calculator/src/components/AmazingCalculator.vue
+++ amazing-calculator/src/components/AmazingCalculator.vue
@@ -11,4 +11,18 @@ const appendToDisplay = (value) => {
     }
 }
 
+const calculate = () => {
+    try {
+        display.value = eval(display.value).toString()
+    } catch (error){
+        display.value = 'Error'
+    }
+}
+
+// computed property for dynamic class binding
+
+const displayClass = computed(() => {
+    return display.value.length > 12 ? 'small-text' : ''
+})
+
 </script>
