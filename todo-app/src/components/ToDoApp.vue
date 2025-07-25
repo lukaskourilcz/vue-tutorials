c34bc19947e95b770a38d801ff7f0aa523df3322
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-25 20:26:02 +0200

    addTask function

---
+++ todo-app/src/components/ToDoApp.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+const newTask = ref('')
+const tasks = ref([])
+
+const addTask = () => {
+    if(newTask.value !== ''){
+        newTask.value.trim.push(tasks)
+        newTask.value =''
+    }
+}
+
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
