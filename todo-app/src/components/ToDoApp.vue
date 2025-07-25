e24c46b7ec4fc481bbac4e5b4db1f130b0a2da37
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-25 20:35:12 +0200

    task list done

--- todo-app/src/components/ToDoApp.vue
+++ todo-app/src/components/ToDoApp.vue
@@ -22,8 +22,15 @@ const removeTask = (index) => {
 <template>
 <div class="todo-app">
     <div class="task-input">
-        <input v-model="newTask" @keyup.enter="addTask" placeholder="add your task here">
+        <input v-model="newTask" @keyup.enter="addTask" placeholder="add your task here"/>
+        <button @click="addTask">Add task</button>
     </div>
+    <ul>
+        <li v-for="(task, index) in tasks" key="index">
+            {{ task }}
+        </li>
+        <button @click="removeTask">Remove</button>
+    </ul>
 </div>
 </template>
 
