6e2d5c632335d46f5d74fa0fcfcfbd49b1730579
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-25 23:55:59 +0200

    buttons done

--- amazing-calculator/src/components/AmazingCalculator.vue
+++ amazing-calculator/src/components/AmazingCalculator.vue
@@ -1,28 +1,60 @@
 <script setup>
-import {ref, computed} from 'vue'
+import { ref, computed } from "vue";
 
-const display = ref('0')
+const display = ref("0");
 
 const appendToDisplay = (value) => {
-    if(display.value === '0' && value !== '.'){
-        display.value = value
-    } else {
-        display.value += value;
-    }
-}
+  if (display.value === "0" && value !== ".") {
+    display.value = value;
+  } else {
+    display.value += value;
+  }
+};
 
 const calculate = () => {
-    try {
-        display.value = eval(display.value).toString()
-    } catch (error){
-        display.value = 'Error'
-    }
-}
+  try {
+    display.value = eval(display.value).toString();
+  } catch (error) {
+    display.value = "Error";
+  }
+};
 
 // computed property for dynamic class binding
 
 const displayClass = computed(() => {
-    return display.value.length > 12 ? 'small-text' : ''
-})
+  return display.value.length > 12 ? "small-text" : "";
+});
 
+const clearDisplay = () => {
+  display.value = "0";
+};
 </script>
+
+<template>
+  <div>
+    <div class="calculator">
+      <input v-model="display" :class="displayClass" readonly />
+      <div class="buttons">
+        <button @click="appendToDisplay('7')">7</button>
+        <button @click="appendToDisplay('8')">8</button>
+        <button @click="appendToDisplay('9')">9</button>
+        <button @click="appendToDisplay('/')">/</button>
+
+        <button @click="appendToDisplay('4')">4</button>
+        <button @click="appendToDisplay('5')">5</button>
+        <button @click="appendToDisplay('6')">6</button>
+        <button @click="appendToDisplay('*')">*</button>
+
+        <button @click="appendToDisplay('1')">1</button>
+        <button @click="appendToDisplay('2')">2</button>
+        <button @click="appendToDisplay('3')">3</button>
+        <button @click="appendToDisplay('-')">-</button>
+
+        <button @click="appendToDisplay('0')">0</button>
+        <button @click="appendToDisplay('.')">.</button>
+        <button @click="appendToDisplay('=')">=</button>
+        <button @click="appendToDisplay('+')">+</button>
+      </div>
+    </div>
+  </div>
+</template>
