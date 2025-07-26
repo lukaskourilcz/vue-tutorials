bdb4474768572d84edb5e2721f38abe6cf0d4a4b
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-26 14:30:39 +0200

    styling done, project done

--- amazing-calculator/src/components/AmazingCalculator.vue
+++ amazing-calculator/src/components/AmazingCalculator.vue
@@ -52,9 +52,54 @@ const clearDisplay = () => {
 
         <button @click="appendToDisplay('0')">0</button>
         <button @click="appendToDisplay('.')">.</button>
-        <button @click="appendToDisplay('=')">=</button>
+        <button @click="calculate()">=</button>
         <button @click="appendToDisplay('+')">+</button>
       </div>
+      <button @click="clearDisplay" class="clear-button">C</button>
     </div>
   </div>
 </template>
+
+<style>
+input {
+  padding: 10px 20px;
+  margin-bottom: 20px;
+}
+
+.calculator {
+  max-width: 300px;
+  margin: 50px auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.display {
+  width: 100%;
+  margin-bottom: 10px;
+  padding: 10px;
+  font-size: 18px;
+  text-align: right;
+}
+
+.buttons {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+}
+
+button {
+  width: 100%;
+  padding: 10px;
+  font-size: 18px;
+}
+
+.clear-button {
+  width: 100%;
+  margin-top: 10px;
+}
+
+.small-text {
+  font-size: 14px;
+}
+</style>
