e19f5ff216b41c49c811c9168135a1dbce13bc0e
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-26 16:06:59 +0200

    scripts done

--- form-validation/src/components/FormValidation.vue
+++ form-validation/src/components/FormValidation.vue
@@ -1,3 +1,29 @@
+// /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
 <script setup>
 
+import { ref, computed } from 'vue';
+
+const formData = ref({
+    name: '',
+    email: '',
+    password: ''
+})
+
+const isNameValid = computed(() => formData.Data.value.name.trim() !== '')
+const isEmailValid = computed(() => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.value.email))
+const isPasswordValid = computed(() => formData.value.password.length >= 8)
+
+const isFormValid = computed(() => isNameValid.value && isEmailValid.value && isPasswordValid.value)
+
+const submitForm = () => {
+    if(isFormValid.value){
+        console.log('Form Submitted', formData.value)
+    } else {
+        console.log('Form is invalid')
+    }
+}
+
 </script>
+
+<template></template>
