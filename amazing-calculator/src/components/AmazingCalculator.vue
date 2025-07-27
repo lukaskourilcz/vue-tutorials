11fc73d5bfd4b98de72a5250be3abbee44cdc9ff
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-27 12:12:08 +0200

    project redone again

--- amazing-calculator/src/components/AmazingCalculator.vue
+++ amazing-calculator/src/components/AmazingCalculator.vue
@@ -1,34 +1,4 @@
-<script setup>
-import { ref } from "vue";
-
-const display = ref("0");
-
-const appendToDisplay = (value) => {
-  if (display.value === "0" && value !== ".") {
-    display.value = value;
-  } else {
-    display.value += value;
-  }
-};
-
-const calculate = () => {
-  try {
-    display.value = eval(display.value).toString();
-  } catch (error) {
-    display.value = "Error";
-  }
-};
-
-const clearDisplay = () => {
-  display.value = "0";
-};
-</script>
-
-<template>
-  <div class="calculator">
-    <input v-model="display" readonly />
-    <div class="buttons">
-      <button @click="appendToDisplay('7')">7</button>
+<!-- <button @click="appendToDisplay('7')">7</button>
       <button @click="appendToDisplay('8')">8</button>
       <button @click="appendToDisplay('9')">9</button>
       <button @click="appendToDisplay('/')">/</button>
@@ -46,39 +16,34 @@ const clearDisplay = () => {
       <button @click="appendToDisplay('0')">0</button>
       <button @click="appendToDisplay('.')">.</button>
       <button @click="calculate()">=</button>
-      <button @click="appendToDisplay('+')">+</button>
-    </div>
-    <button @click="clearDisplay" class="clear-button">C</button>
-  </div>
-</template>
+      <button @click="appendToDisplay('+')">+</button> -->
 
-<style scoped>
-.calculator {
-  max-width: 400px;
-  border: 1px solid;
-  padding: 20px;
-  margin: 50px auto;
-}
+<script setup>
+import { ref } from "vue";
 
-input {
-  width: 71%;
-  margin-bottom: 5px;
-  text-align: right;
-  padding: 5px;
-}
+const display = ref("0");
 
-.buttons {
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 5px;
-  margin-bottom: 5px;
-}
+const appendToDisplay = (value) => {
+  if (display.value === "0" && value !== ".") {
+    display.value = value;
+  } else {
+    display.value += value;
+  }
+};
 
-button {
-  padding: 5px;
-}
+const calculate = () => {
+  try {
+    display.value = eval(display.value).toString();
+  } catch (error) {
+    display.value = "ERROR";
+  }
+};
 
-.clear-button {
-  width: 100%;
-}
-</style>
+const clearDisplay = () => {
+  display.value = "0";
+};
+</script>
+
+<template></template>
+
+<style scoped></style>
