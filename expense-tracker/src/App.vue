21fab3ac014f1bae83258d7163fba2a7f2c22fa9
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-27 16:18:27 +0200

    form done, toast added

--- expense-tracker/src/App.vue
+++ expense-tracker/src/App.vue
@@ -5,6 +5,7 @@ import Balance from "./components/Balance.vue";
 import Header from "./components/Header.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
+import { useToast } from "vue-toastification";
 
 const transactions = ref([
   { id: 1, text: "Flower", amount: -9.99 },
@@ -13,6 +14,8 @@ const transactions = ref([
   { id: 4, text: "Rent", amount: 3800 },
   { id: 5, text: "Birthday money", amount: 200 },
 ]);
+
+const toast = useToast()
 
 const total = computed(() => {
   return transactions.value.reduce((acc, transaction) => {
@@ -37,9 +40,20 @@ const expense = computed(() => {
 });
 
 const handleTransactionSubmitted = (transactionData) => {
-  console.log(transactionData)
+
+  transactions.value.push({
+    id: generateUniqueId(),
+    text:transactionData.text,
+    amount: transactionData.amount
+  })
+
+  toast.success('Transaction addded to the list, mijo.')
 }
 
+const generateUniqueId = () => {
+  return Math.floor(Math.random()*1000)
+}
+ 
 </script>
 
 <template>
