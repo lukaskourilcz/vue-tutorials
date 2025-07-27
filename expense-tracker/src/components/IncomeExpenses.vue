0b641b1ddc3d18ef74b4e17f9ff9aa8c22d47e5a
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-27 14:47:08 +0200

    transaction-list

--- expense-tracker/src/components/IncomeExpenses.vue
+++ expense-tracker/src/components/IncomeExpenses.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="inc-exp--container">
+  <div class="inc-exp-container">
     <div>
       <h4>Income</h4>
       <p id="money-plus" class="money plus">+$0.00</p>
---
+++ expense-tracker/src/components/TransactionList.vue
@@ -0,0 +1,15 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li class="minus">
+      Cash
+      <span>-$400</span>
+      <button class="delete-button">x</button>
+    </li>
+    <li class="plus">
+      Paycheck
+      <span>$800</span>
+      <button class="delete-button">x</button>
+    </li>
+  </ul>
+</template>
