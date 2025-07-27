5c37899a3c17e8f94b061c6a6a5b81666cee357a
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-27 13:17:57 +0200

    income-expense

--- expense-tracker/src/App.vue
+++ expense-tracker/src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
+import Balance from './components/Balance.vue';
 import Header from './components/Header.vue';
 </script>
 
 <template>
 <Header />
 <div class="container">
-  
+  <Balance />
 </div>
 </template>
 
