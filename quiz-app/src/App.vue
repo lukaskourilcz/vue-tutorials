488b8db82a4a453dfe29f78aa28e176479548abe
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-27 17:11:28 +0200

    questions arr added to App.vue

--- quiz-app/src/App.vue
+++ quiz-app/src/App.vue
@@ -1,9 +1,25 @@
 <script setup>
-import QuizCard from './components/QuizCard.vue';
+import QuizCard from "./components/QuizCard.vue";
+import { ref } from "vue";
 
-
+const questions = ref([
+  {
+    text: "K čemu slouží `ref()` ve Vue?",
+    options: [
+      "K načtení externích dat",
+      "K reaktivnímu uchování hodnoty",
+      "K definici komponenty",
+      "K přístupu k API",
+    ],
+    correct: 1,
+  },
+]);
 </script>
 
 <template>
-  <QuizCard />
+  <div class="container mt-5">
+    <h1 class="mb-4">Vue & JS Quiz</h1>
+
+    <QuizCard :question="questions[0]" />
+  </div>
 </template>
