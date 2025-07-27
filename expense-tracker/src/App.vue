c835e184bb78a984e8829d9cd3b9119b12ba699b
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-27 15:16:04 +0200

    defineProps implemented

--- expense-tracker/src/App.vue
+++ expense-tracker/src/App.vue
@@ -1,9 +1,21 @@
 <script setup>
+import { ref } from 'vue';
 import AddTransaction from './components/AddTransaction.vue';
 import Balance from './components/Balance.vue';
 import Header from './components/Header.vue';
 import IncomeExpenses from './components/IncomeExpenses.vue';
 import TransactionList from './components/TransactionList.vue';
+
+
+const transactions = ref([
+  { id: 1, text: "Flower", amount: -9.99 },
+  { id: 2, text: "House key", amount: -1.99 },
+  { id: 3, text: "Car", amount: -1999.99 },
+  { id: 4, text: "Rent", amount: 8000 },
+  { id: 5, text: "Birthday money", amount: 200 },
+]);
+
+
 </script>
 
 <template>
@@ -11,7 +23,7 @@ import TransactionList from './components/TransactionList.vue';
 <div class="container">
   <Balance />
   <IncomeExpenses />
-  <TransactionList />
+  <TransactionList :transactions="transactions" />
   <AddTransaction />
 </div>
 </template>
--- expense-tracker/src/components/TransactionList.vue
+++ expense-tracker/src/components/TransactionList.vue
@@ -1,13 +1,14 @@
 <script setup>
-import { ref } from "vue";
 
-const transactions = ref([
-  { id: 1, text: "Flower", amount: -9.99 },
-  { id: 2, text: "House key", amount: -1.99 },
-  { id: 3, text: "Car", amount: -1999.99 },
-  { id: 4, text: "Rent", amount: 8000 },
-  { id: 5, text: "Birthday money", amount: 200 },
-]);
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    transactions: {
+        type: Array,
+        required: true
+    }
+})
+
 </script>
 
 <template>
@@ -19,7 +20,7 @@ const transactions = ref([
       :class="transaction.amount < 0 ? 'minus' : 'plus'"
     >
       {{ transaction.text }}
-      <span>${{ transaction.amount }}</span>
+      <span>$ {{ transaction.amount }}</span>
       <button class="delete-btn">x</button>
     </li>
   </ul>
