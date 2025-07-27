d3acb2c2e3fe371504546a722ebe356296e646cf
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-27 17:43:16 +0200

    explanation field added, score added

--- quiz-app/src/components/QuizCard.vue
+++ quiz-app/src/components/QuizCard.vue
@@ -1,40 +1,55 @@
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, watch } from "vue";
 
 const props = defineProps({
   question: Object,
 });
 
+const emit = defineEmits(['answered'])
+
+
 const selected = ref(null);
 
-const isCorrect = computed(() => {
-  return selected.value === question.correct;
-});
+const isCorrect = computed(() => selected.value === props.question.correct);
 
 const selectOption = (index) => {
-  selected.value = index;
+  if (selected.value === null) {
+    selected.value = index;
+    emit("answered", index === props.question.correct);
+  }
 };
+
+watch(
+  () => props.question,
+  () => {
+    selected.value = null;
+  }
+);
 </script>
 
 <template>
   <div class="card p-3 mb-3">
-    <h5>Question text</h5>
+    <h5>{{ question.text }}</h5>
     <ul class="list-group">
       <li
         v-for="(option, index) in question.options"
-        @click="selectOption(index)"
         :key="index"
         class="list-group-item"
         :class="{
-          'list-group-item-success': selected === index && isCorrect,
-          'list-group-item-danger': selected === index && !isCorrect,
+          'list-group-item-success':
+            selected !== null && index === question.correct,
+          'list-group-item-danger':
+            selected === index && index !== question.correct,
         }"
+        @click="selectOption(index)"
       >
         {{ option }}
       </li>
     </ul>
-    <div v-if="selected !== null" class="mt-2">
-      <strong>{{ isCorrect ? "Correct" : "Wrong" }}</strong>
+
+    <div v-if="selected !== null" class="mt-3">
+      <strong>{{ isCorrect ? "Správně!" : "Špatně!" }}</strong>
+      <p class="mt-2 text-muted">{{ question.explanation }}</p>
     </div>
   </div>
 </template>
