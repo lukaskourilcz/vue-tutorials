4168216cbc0d528f93940629f8df4429f531d8c2
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-27 15:12:07 +0200

    transaction list function done

--- expense-tracker/src/components/TransactionList.vue
+++ expense-tracker/src/components/TransactionList.vue
@@ -1,15 +1,26 @@
+<script setup>
+import { ref } from "vue";
+
+const transactions = ref([
+  { id: 1, text: "Flower", amount: -9.99 },
+  { id: 2, text: "House key", amount: -1.99 },
+  { id: 3, text: "Car", amount: -1999.99 },
+  { id: 4, text: "Rent", amount: 8000 },
+  { id: 5, text: "Birthday money", amount: 200 },
+]);
+</script>
+
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li class="minus">
-      Cash
-      <span>-$400</span>
-      <button class="delete-button">x</button>
-    </li>
-    <li class="plus">
-      Paycheck
-      <span>$800</span>
-      <button class="delete-button">x</button>
+    <li
+      v-for="transaction in transactions"
+      :key="transactions.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }}
+      <span>${{ transaction.amount }}</span>
+      <button class="delete-btn">x</button>
     </li>
   </ul>
 </template>
