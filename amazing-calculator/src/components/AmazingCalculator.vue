1f771618b48ab8c8899c50d707023da21f1fbb8b
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-27 11:07:01 +0200

    project redone completed

--- amazing-calculator/src/components/AmazingCalculator.vue
+++ amazing-calculator/src/components/AmazingCalculator.vue
@@ -1,37 +1,93 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
 
-const display = ref('0')
+const display = ref("0");
 
 const appendToDisplay = (value) => {
-  if(display === '0' && value !== '.' ){
-    display.value = value
+  if (display.value === "0" && value !== ".") {
+    display.value = value;
   } else {
-    display.value += value
+    display.value += value;
   }
-}
+};
 
 const calculate = () => {
   try {
-    display.value = eval(display.value).toString()
-  } catch (error){
-    display.value = 'Error'
+    display.value = eval(display.value).toString();
+  } catch (error) {
+    display.value = "Error";
   }
-}
+};
 
-
+const clearDisplay = () => {
+  display.value = "0";
+};
 </script>
 
 <template>
+  <div class="calculator">
+    <div>
+      <input v-model="display" readonly />
+    </div>
+    <div class="buttons">
+      <button @click="appendToDisplay('7')">7</button>
+      <button @click="appendToDisplay('8')">8</button>
+      <button @click="appendToDisplay('9')">9</button>
+      <button @click="appendToDisplay('/')">/</button>
 
+      <button @click="appendToDisplay('4')">4</button>
+      <button @click="appendToDisplay('5')">5</button>
+      <button @click="appendToDisplay('6')">6</button>
+      <button @click="appendToDisplay('*')">*</button>
+
+      <button @click="appendToDisplay('1')">1</button>
+      <button @click="appendToDisplay('2')">2</button>
+      <button @click="appendToDisplay('3')">3</button>
+      <button @click="appendToDisplay('-')">-</button>
+
+      <button @click="appendToDisplay('0')">0</button>
+      <button @click="appendToDisplay('.')">.</button>
+      <button @click="calculate()">=</button>
+      <button @click="appendToDisplay('+')">+</button>
+    </div>
+    <button @click="clearDisplay">C</button>
+  </div>
 </template>
 
 <style scoped>
+.calculator {
+  max-width: 400px;
+  border: 1px solid;
+  margin: 50px auto;
+  padding: 20px;
+}
+
+input {
+ width: 68%;
+ padding: 10px;
+ text-align: right;
+ margin-bottom: 10px;
+}
+
+.buttons {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+  margin-bottom: 10px;
+}
+
+button {
+  width: 100%;
+  padding: 10px;
+  font-size: 18px;
+}
+
+.clear-button {
+  width: 100%;
+  margin-top: 10px;
+}
 
 </style>
-
-
-
 
 <!-- <script setup>
 import { ref, computed } from "vue";
