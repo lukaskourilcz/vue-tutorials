c9a69b3e6f50eee293edbc506c1ab9dff1117573
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-27 16:04:34 +0200

    toastification added

--- expense-tracker/src/main.js
+++ expense-tracker/src/main.js
@@ -1,5 +1,10 @@
-import './assets/style.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+import './assets/style.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(Toast)
+app.mount('#app')
