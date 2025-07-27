0217500d2c2e7e91a90ba5d56c33155e850aba2b
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-27 17:03:28 +0200

    quiz card script done

--- quiz-app/src/components/QuizCard.vue
+++ quiz-app/src/components/QuizCard.vue
@@ -1,5 +1,19 @@
 <script setup>
+import { ref, computed } from "vue";
 
+const props = defineProps({
+  question: Object,
+});
+
+const selected = ref(null);
+
+const isCorrect = computed(() => {
+  return selected.value === question.correct;
+});
+
+const selectOption = (index) => {
+  selected.value = index;
+};
 </script>
 
 <template></template>
