658f4d9fa6f46984c61c9048ffeefca169d108fc
Lukas Kouril <kouril.lukas@gmail.com>
2025-07-28 10:38:51 +0200

    project redone finished

--- quote-generator/src/components/RandomQuoteGenerator.vue
+++ quote-generator/src/components/RandomQuoteGenerator.vue
@@ -1,6 +1,9 @@
 // 1. Vytvorit consts pro seznam quotes, currentQuote // 2. Vytvorit funkci pro
 random quote // 3. Vytvorit html skeleton // 4. Styling
 
+<script setup>
+import { ref, onMounted } from "vue";
+
 const quotes = ref([
   {
     text: "Life is what happens when you're busy making other plans.",
@@ -31,3 +34,48 @@ const quotes = ref([
     author: "Oscar Wilde",
   },
 ]);
+
+const currentQuote = ref({ text: "", author: "" });
+
+const getRandomQuote = () => {
+  const randomIndex = Math.floor(Math.random() * quotes.value.length);
+  currentQuote.value = quotes.value[randomIndex];
+};
+
+onMounted(getRandomQuote);
+</script>
+
+<template>
+  <div class="quote-gen">
+    <div>
+      <blockquote>
+        <p>{{ currentQuote.text }}</p>
+        <cite>{{ currentQuote.author }}</cite>
+      </blockquote>
+    </div>
+    <button @click="getRandomQuote">Get another quote</button>
+  </div>
+</template>
+
+<style scoped>
+.quote-gen {
+  max-width: 400px;
+  margin: 50px auto;
+  border: 1px solid;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+}
+
+blockquote {
+  margin-bottom: 24px;
+}
+
+cite {
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 10px;
+}
+</style>
